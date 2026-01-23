5fca8a96f98ff80ddceca099c8a5d6fde37722a0
Joe Castro <28631955+joematrix77@users.noreply.github.com>
2026-01-23 00:30:32 -0500

    Update LinuxROCmSetup.sh

--- LinuxROCmSetup.sh
+++ LinuxROCmSetup.sh
@@ -65,8 +65,14 @@ curl -L -A "Mozilla/5.0" https://repo.radeon.com | \
 gpg --dearmor | sudo tee /etc/apt/keyrings/rocm.gpg > /dev/null
 
 sudo tee /etc/apt/sources.list.d/rocm.list << EOF
-deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com noble main
-deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com noble main
+deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/7.2 noble main
+deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/graphics/7.2/ubuntu noble main
+EOF
+
+sudo tee /etc/apt/preferences.d/rocm-pin-600 << EOF
+Package: *
+Pin: release o=repo.radeon.com
+Pin-Priority: 600
 EOF
 
 sudo apt update
